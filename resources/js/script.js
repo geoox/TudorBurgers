7bd0d85b85493b9d57503b15b702708244927dd7
George Dobrin <georgedobrin11@gmail.com>
2018-07-15 01:54:22 +0300

    asa3

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,21 +2,9 @@ $(document).ready(function(){
     $('.carousel').carousel();
     $('.slider').slider();
     $('.modal').modal();
-
-    var array=document.getElementsByClassName('logoSlider');
-    for(let i=0;i<array.length;i++){
-      //  array[i].setAttribute('style','filter:blur(0px)');
-    }
     
 });
 
-$('#bucuresti').click(function(e) {
-  e.preventDefault();
-$('body').css('overflow', 'hidden');
-});
-
-$('#bucuresti').click(function(e) {
-  e.preventDefault();
-$('body').css('overflow', 'auto');
-
-});
+$('#bucuresti-modal').click(function(){
+  $('body').css('overflow','hidden');
+})
