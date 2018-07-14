949da55c82c9b544284dd87cadf9630833116fe2
George Dobrin <georgedobrin11@gmail.com>
2018-07-14 22:39:41 +0300

    added blur intro

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
     $('.carousel').carousel();
     $('.slider').slider();
     $('.modal').modal();
-    
-  });
 
-  
+    var array=document.getElementsByClassName('logoSlider');
+    for(let i=0;i<array.length;i++){
+      //  array[i].setAttribute('style','filter:blur(0px)');
+    }
+    
+});
+
+
